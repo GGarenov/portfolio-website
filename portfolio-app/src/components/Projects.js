59d93975fd377e02218cc6f790519dafdfe30f79
Georgiy Garenov <g_garenov@outlook.com>
2023-11-17 21:59:17 +0200

    Real projets were imported in the projects.js - react router dom was installed

--- portfolio-app/src/components/Projects.js
+++ portfolio-app/src/components/Projects.js
@@ -1,8 +1,9 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg2 from "../assets/img/project2.png";
+import projImg3 from "../assets/img/project3.png";
+import projImg4 from "../assets/img/project4.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 
@@ -26,7 +27,7 @@ export const Projects = () => {
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg1,
+      imgUrl: projImg4,
     },
     {
       title: "Business Startup",
