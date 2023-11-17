67e7e7ab43241408eb5f8e02a558a27d76443a03
Georgiy Garenov <g_garenov@outlook.com>
2023-11-17 21:05:46 +0200

    Projects edited version

--- portfolio-app/src/components/Projects.js
+++ portfolio-app/src/components/Projects.js
@@ -51,9 +51,8 @@ export const Projects = () => {
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <h2>Projects</h2>
                   <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book.
+                    Explore a showcase of my projects, a testament to my skills in React, Vanilla JavaScript, Node.js,
+                    Express, MongoDB, Firebase, and Git.
                   </p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav
