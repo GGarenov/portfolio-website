1668ccd3975c11ec50871beb08c60d37a2b6d767
Georgiy Garenov <g_garenov@outlook.com>
2023-11-17 22:42:49 +0200

    Links in the footer were added.

--- portfolio-app/src/components/Footer.js
+++ portfolio-app/src/components/Footer.js
@@ -1,8 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/edited.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
@@ -14,10 +13,10 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#">
+              <a href="https://www.linkedin.com/in/ggarenov/" target="_blank" rel="noopener noreferrer">
                 <img src={navIcon1} alt="Icon" />
               </a>
-              <a href="#">
+              <a href="https://github.com/GGarenov" target="_blank" rel="noopener noreferrer">
                 <img src={navIcon2} alt="Icon" />
               </a>
             </div>
