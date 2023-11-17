021bc171fcdab56e46c0968ca801fb896b2189fb
Georgiy Garenov <g_garenov@outlook.com>
2023-11-17 20:46:47 +0200

    First project was successfully imported

--- portfolio-app/src/components/Projects.js
+++ portfolio-app/src/components/Projects.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
+import projImg1 from "../assets/img/project1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
@@ -10,8 +10,8 @@ import TrackVisibility from "react-on-screen";
 export const Projects = () => {
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Real Estate App",
+      description: "Build with Express, React and MongoDB",
       imgUrl: projImg1,
     },
     {
