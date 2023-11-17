4bf30d8b0cce1ed81dfc0a47de579c3dce69277b
Georgiy Garenov <g_garenov@outlook.com>
2023-11-17 21:28:47 +0200

    Tabs were commented out because right now I don't need them

--- portfolio-app/src/components/Projects.js
+++ portfolio-app/src/components/Projects.js
@@ -5,7 +5,6 @@ import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
-import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
   const projects = [
@@ -46,57 +45,53 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility>
-              {({ isVisible }) => (
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <h2>Projects</h2>
-                  <p>
-                    Explore a showcase of my projects, a testament to my skills in React, Vanilla JavaScript, Node.js,
-                    Express, MongoDB, Firebase, and Git.
-                  </p>
-                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                    <Nav
-                      variant="pills"
-                      className="nav-pills mb-5 justify-content-center align-items-center"
-                      id="pills-tab"
-                    >
-                      <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                      </Nav.Item>
-                    </Nav>
-                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                      <Tab.Pane eventKey="first">
-                        <Row>
-                          {projects.map((project, index) => {
-                            return <ProjectCard key={index} {...project} />;
-                          })}
-                        </Row>
-                      </Tab.Pane>
-                      <Tab.Pane eventKey="section">
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident
-                          velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis
-                          asperiores delectus quasi inventore debitis quo.
-                        </p>
-                      </Tab.Pane>
-                      <Tab.Pane eventKey="third">
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident
-                          velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis
-                          asperiores delectus quasi inventore debitis quo.
-                        </p>
-                      </Tab.Pane>
-                    </Tab.Content>
-                  </Tab.Container>
-                </div>
-              )}
-            </TrackVisibility>
+            <div>
+              <h2>Projects</h2>
+              <p>
+                Explore a showcase of my projects, a testament to my skills in React, Vanilla JavaScript, Node.js,
+                Express, MongoDB, Firebase, and Git.
+              </p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                {/* <Nav
+                  variant="pills"
+                  className="nav-pills mb-5 justify-content-center align-items-center"
+                  id="pills-tab"
+                >
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                  </Nav.Item>
+                </Nav> */}
+                <Tab.Content id="slideInUp">
+                  <Tab.Pane eventKey="first">
+                    <Row>
+                      {projects.map((project, index) => {
+                        return <ProjectCard key={index} {...project} />;
+                      })}
+                    </Row>
+                  </Tab.Pane>
+                  {/* <Tab.Pane eventKey="section">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit,
+                      rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus
+                      quasi inventore debitis quo.
+                    </p>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="third">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit,
+                      rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus
+                      quasi inventore debitis quo.
+                    </p>
+                  </Tab.Pane> */}
+                </Tab.Content>
+              </Tab.Container>
+            </div>
           </Col>
         </Row>
       </Container>
