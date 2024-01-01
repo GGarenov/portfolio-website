5997d2f638a93433a92b9011041492ea249d90f3
Georgiy Garenov <g_garenov@outlook.com>
2024-01-01 14:49:21 +0200

    Buttons for github source code and preview if the app is deployed somewhere.

--- portfolio-app/src/components/ProjectCard.js
+++ portfolio-app/src/components/ProjectCard.js
@@ -1,18 +1,26 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, Button } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl, link }) => {
+export const ProjectCard = ({ title, description, imgUrl, githubLink, previewLink }) => {
   return (
     <Col sm={6} md={4}>
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <div className="proj-imgbx">
-          <img src={imgUrl} alt={title} />
-          <div className="proj-txtx">
-            <h4>{title}</h4>
-            <span>{description}</span>
+      <div className="proj-imgbx">
+        <img src={imgUrl} alt={title} />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+          <div className="mt-10">
+            <Button href={githubLink} target="_blank" rel="noopener noreferrer" className="mr-10">
+              GitHub
+            </Button>
+            {previewLink && (
+              <Button href={previewLink} target="_blank" rel="noopener noreferrer">
+                Preview
+              </Button>
+            )}
           </div>
         </div>
-      </a>
+      </div>
     </Col>
   );
 };
